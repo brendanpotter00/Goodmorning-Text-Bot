16d6a3696496645f445cd9e724c5596790d7fa09
bpotter-trinity <bpotter@trinity.edu>
2021-06-03 15:39:28 -0500

    done!

--- app.py
+++ app.py
@@ -7,13 +7,15 @@ import sys
 
 mynumber = 3617396315
 marielNumber = 9493455203
+hourToSend = 9
+delayToLoop = 300
 endProgram = False
 alarm = datetime.time(9, 1, 1) #Hour, minute and second you want.
 
 def send_message():
     resp = requests.post('https://textbelt.com/text', {
         'phone' : marielNumber,
-        'message' : 'Goodmorning baby :) Im asleep rn, but have a good day!',
+        'message' : 'Good afternoon baby :) Im testing my program, but have a good rest of your day!',
         'key' : 'textbelt'
     })
     print (resp.json())
@@ -21,7 +23,7 @@ def send_message():
 #main call
 #schedule.every().day.at('09:00').do(send_message)
 
-schedule.every(1).seconds.do(send_message)
+schedule.every(3).seconds.do(send_message)
 
 #print('hello')
 now = datetime.datetime.now()
@@ -34,16 +36,16 @@ second = now.second
 """ hour = 9 
 minute = 16  """
 
-#testing NOT NEEDED
+""" #testing NOT NEEDED
 def clock():
     while True:
         print(datetime.datetime.now().strftime("%H:%M:%S"), end="\r")
         time.sleep(1)
 
-#clock()
+#clock() """
 
-while True or hour == 9 :
-    if hour == 9 and minute > 15 :
+while True or hour == hourToSend :
+    if hour == hourToSend and minute >= 59 :
         endProgram = True
         print("END IS TRUE")
 
@@ -52,9 +54,9 @@ while True or hour == 9 :
     second = datetime.datetime.now().second
 
     print("beginning of loop")
-    time.sleep(3) # 5minute delay
+    time.sleep(delayToLoop) # 5 minute delay
     print("after delay MIDDLE")
-    if hour == 9 and minute < 15 :
+    if hour == hourToSend and minute <= 59 :
         print("SENDING TEXT")
         schedule.run_pending()
     
